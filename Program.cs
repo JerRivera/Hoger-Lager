c81919bd776f2cd28d32dcbc9e0b94ec727402dc
dvanderblom <70724238+dvanderblom@users.noreply.github.com>
2021-02-03 14:36:20 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hoger_Lager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
